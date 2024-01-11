d10e88e8acef92f03471995cef5278d4808e2931
nb <nate.buckareff@gmail.com>
2024-01-11 14:23:09 -0800

    Framebuffer image views should not outlive framebuffer

--- src/gpu/framebuffer.rs
+++ src/gpu/framebuffer.rs
@@ -5,12 +5,13 @@ use std::sync::Arc;
 pub struct Framebuffer {
     render_pass: Arc<RenderPass>,
     vk_framebuffer: vk::Framebuffer,
+    image_views: Vec<Arc<ImageView>>,
 }
 
 impl Framebuffer {
     pub fn new(
         render_pass: &Arc<RenderPass>,
-        image_views: &Vec<Arc<ImageView>>,
+        image_views: Vec<Arc<ImageView>>,
         width: u32,
         height: u32,
         layers: u32,
@@ -45,6 +46,7 @@ impl Framebuffer {
         Arc::new(Framebuffer {
             render_pass: render_pass.clone(),
             vk_framebuffer,
+            image_views,
         })
     }
 
