e84625f8bc513e6197da38372515d2175fb6100a
nb <nate.buckareff@gmail.com>
2024-01-11 14:24:38 -0800

    Refactor rendering out of main
    `RenderContext` holds global rendering state. `RenderFrame` holds state for inflight frames.

---
+++ src/render_context.rs
@@ -0,0 +1,444 @@
+use ash::vk;
+use std::{rc::Rc, sync::Arc};
+use winit::window::Window;
+
+const MAX_FRAMES_IN_FLIGHT: usize = 1;
+
+use crate::gpu::{
+    CommandBuffer, CommandPool, Device, Fence, Framebuffer, GraphicsPipeline, Instance,
+    PhysicalDevice, PipelineLayout, RenderPass, RenderPassConfig, Semaphore, ShaderKind,
+    ShaderModule, Swapchain,
+};
+
+pub struct RenderContext {
+    instance: Arc<Instance>,
+    physical_device: Arc<PhysicalDevice>,
+    device: Arc<Device>,
+    swapchain: Arc<Swapchain>,
+    shader_modules: Vec<Arc<ShaderModule>>,
+    render_pass: Arc<RenderPass>,
+    graphics_pipeline: Arc<GraphicsPipeline>,
+    framebuffers: Vec<Arc<Framebuffer>>,
+    cmd_pool: Rc<CommandPool>,
+    render_frames: Vec<RenderFrame>,
+}
+
+impl RenderContext {
+    pub fn new(window: &Arc<Window>) -> Self {
+        let instance = Instance::new(&window);
+
+        let required_queue_flags = vec![vk::QueueFlags::GRAPHICS];
+
+        let required_extensions: Vec<String> = vec![
+            // String::from("VK_EXT_debug_utils"),
+            String::from("VK_KHR_swapchain"),
+        ];
+
+        // Find the first physical device that supports the swapchain extension
+        // and is preferably a discrete GPU
+        let physical_device = instance
+            .get_physical_devices()
+            .into_iter()
+            .min_by_key(|x| match x.device_type() {
+                // Rank each physical device by the type, with preference for discrete
+                vk::PhysicalDeviceType::DISCRETE_GPU => 0,
+                vk::PhysicalDeviceType::INTEGRATED_GPU => 1,
+                _ => 4,
+            })
+            .filter(|x| {
+                // Filter for physical devices that support all of the required
+                // queue flags and the window surface
+                let mut supports_surface = false;
+                let mut flags = required_queue_flags.clone();
+                for (i, properties) in x.get_queue_family_properties().iter().enumerate() {
+                    if x.supports_surface(i.try_into().unwrap()) {
+                        supports_surface = true;
+                    }
+                    flags.retain(|x| !properties.queue_flags.contains(*x));
+                }
+                supports_surface && flags.len() == 0
+            })
+            .filter(|x| {
+                // Filter for physical devices that support all of the required
+                // extensions
+                let extensions_hashset = x.extension_name_hashset();
+                for x in &required_extensions {
+                    if !extensions_hashset.contains(x) {
+                        return false;
+                    }
+                }
+                true
+            })
+            .unwrap();
+
+        println!("physical_device.name = {}", physical_device.device_name());
+
+        // Use MAILBOX if the device supports it, otherwise fallback to FIFO
+        let surface_present_mode = physical_device
+            .get_surface_present_modes()
+            .into_iter()
+            .min_by_key(|x| match *x {
+                vk::PresentModeKHR::MAILBOX => 0,
+                vk::PresentModeKHR::FIFO => 1,
+                _ => 2,
+            })
+            .unwrap();
+
+        // Chose the swapchain surface format to use, preferring B8G8R8A8_SRGB
+        // with a SRGB_NONLINEAR color space, and otherwise taking the first
+        // option
+        let surface_format = physical_device
+            .get_surface_formats()
+            .into_iter()
+            .enumerate()
+            .min_by_key(|(index, x)| {
+                if x.format == vk::Format::B8G8R8A8_SRGB {
+                    if x.color_space == vk::ColorSpaceKHR::SRGB_NONLINEAR {
+                        return 0;
+                    }
+                }
+                index + 1
+            })
+            .map(|(_, x)| x)
+            .unwrap();
+
+        let surface_extent =
+            physical_device.get_surface_current_extent_clamped(window.inner_size());
+
+        println!("surface_present_mode = {:?}", surface_present_mode);
+        println!("surface_present_format = {:?}", surface_format);
+        println!("surface_extent = {:?}", surface_extent);
+
+        // Select queue family indices for logical device creation
+        let mut queue_family_indices = vec![];
+        for (i, x) in physical_device
+            .get_queue_family_properties()
+            .iter()
+            .enumerate()
+        {
+            println!(
+                "queue_family[{}] = (queue_count = {}, {:?})",
+                i, x.queue_count, x.queue_flags
+            );
+
+            let mut enable = false;
+
+            if x.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
+                enable = true;
+            }
+
+            if x.queue_flags.contains(vk::QueueFlags::COMPUTE) {
+                enable = true;
+            }
+
+            if enable {
+                let queue_family_index: u32 = i.try_into().unwrap();
+                queue_family_indices.push(queue_family_index);
+            }
+        }
+
+        let device = physical_device.get_device(&queue_family_indices, &required_extensions);
+
+        let swapchain = device.get_swapchain(
+            physical_device.get_surface_ideal_image_count(),
+            surface_format.format,
+            surface_format.color_space,
+            surface_extent,
+            vk::ImageUsageFlags::COLOR_ATTACHMENT,
+            surface_present_mode,
+            None,
+        );
+
+        let swapchain_images = swapchain.images();
+
+        let swapchain_image_views = swapchain_images
+            .iter()
+            .map(|image| {
+                image.get_image_view(
+                    vk::ImageViewType::TYPE_2D,
+                    *swapchain.format(),
+                    vk::ComponentMapping::default(),
+                    vk::ImageSubresourceRange {
+                        aspect_mask: vk::ImageAspectFlags::COLOR,
+                        base_mip_level: 0,
+                        level_count: 1,
+                        base_array_layer: 0,
+                        layer_count: 1,
+                    },
+                )
+            })
+            .collect::<Vec<_>>();
+
+        println!(
+            "swapchain_image_views.len() = {}",
+            swapchain_image_views.len()
+        );
+
+        let shader_compiler = shaderc::Compiler::new().unwrap();
+
+        let shader_modules = vec![
+            ShaderModule::new(
+                &device,
+                &shader_compiler,
+                include_str!("./shaders/vertex.glsl"),
+                ShaderKind::Vertex,
+                "vertex.glsl",
+                "main",
+                None,
+            ),
+            ShaderModule::new(
+                &device,
+                &shader_compiler,
+                include_str!("./shaders/fragment.glsl"),
+                ShaderKind::Fragment,
+                "fragment.glsl",
+                "main",
+                None,
+            ),
+        ];
+
+        let render_pass = {
+            let mut builder = RenderPass::builder();
+
+            let color = builder
+                .attachment()
+                .format(*swapchain.format())
+                .samples(vk::SampleCountFlags::TYPE_1)
+                .load_op(vk::AttachmentLoadOp::CLEAR)
+                .store_op(vk::AttachmentStoreOp::STORE)
+                .initial_layout(vk::ImageLayout::UNDEFINED)
+                .final_layout(vk::ImageLayout::PRESENT_SRC_KHR);
+
+            let subpass = builder
+                .subpass()
+                .color(&color, vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL);
+
+            let dep = builder
+                .dependency()
+                .src_external()
+                .src_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+                .dst(&subpass)
+                .dst_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+                .dst_access_mask(vk::AccessFlags::COLOR_ATTACHMENT_WRITE);
+
+            builder.build(RenderPassConfig {
+                device: &device,
+                attachments: vec![color],
+                subpasses: vec![subpass],
+                dependencies: Some(vec![dep]),
+            })
+        };
+
+        let pipeline_layout = PipelineLayout::new(&device, None, None);
+
+        let graphics_pipeline = GraphicsPipeline::new(
+            &device,
+            &shader_modules,
+            &vec![vk::DynamicState::VIEWPORT, vk::DynamicState::SCISSOR],
+            vk::PrimitiveTopology::TRIANGLE_LIST,
+            false,
+            None,
+            None,
+            &pipeline_layout,
+            &render_pass,
+        );
+
+        let mut framebuffers = vec![];
+
+        for image_view in &swapchain_image_views {
+            let vk::Extent2D { width, height } = *swapchain.extent();
+            framebuffers.push(Framebuffer::new(
+                &render_pass,
+                vec![image_view.clone()],
+                width,
+                height,
+                1,
+            ));
+        }
+
+        println!("framebuffers.len() = {}", framebuffers.len());
+
+        let graphics_queue = device.get_first_queue(vk::QueueFlags::GRAPHICS).unwrap();
+
+        let cmd_pool = CommandPool::new(
+            &device,
+            graphics_queue.queue_family(),
+            vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER,
+        );
+
+        let mut render_context = Self {
+            instance,
+            physical_device,
+            device,
+            swapchain,
+            shader_modules,
+            render_pass,
+            graphics_pipeline,
+            framebuffers,
+            cmd_pool,
+            render_frames: vec![],
+        };
+
+        render_context.render_frames.reserve(MAX_FRAMES_IN_FLIGHT);
+        for _ in 0..MAX_FRAMES_IN_FLIGHT {
+            render_context
+                .render_frames
+                .push(RenderFrame::new(&render_context));
+        }
+
+        render_context
+    }
+
+    pub fn draw_frame(&self) {
+        self.render_frames[0].draw_frame(self);
+    }
+}
+
+impl Drop for RenderContext {
+    fn drop(&mut self) {
+        // Wait for GPU to finish all pending work before dropping the render
+        // context. This gives command buffers time to finish before we drop any
+        // resources they may be referencing
+        self.device.wait_idle();
+    }
+}
+
+struct RenderFrame {
+    cmd_buf: CommandBuffer,
+    image_available: Semaphore,
+    render_finished: Semaphore,
+    in_flight: Fence,
+}
+
+impl RenderFrame {
+    pub fn new(context: &RenderContext) -> Self {
+        let cmd_buf = context
+            .cmd_pool
+            .allocate_one(vk::CommandBufferLevel::PRIMARY);
+
+        let image_available = Semaphore::new(&context.device);
+        let render_finished = Semaphore::new(&context.device);
+        let in_flight = Fence::signaled(&context.device);
+
+        Self {
+            cmd_buf,
+            image_available,
+            render_finished,
+            in_flight,
+        }
+    }
+
+    pub fn draw_frame(&self, context: &RenderContext) {
+        let fences = &[&self.in_flight];
+        context.device.wait_for_fences(fences, true, None);
+        context.device.reset_fences(fences);
+
+        let acquire_result =
+            context
+                .swapchain
+                .acquire_next_image(None, Some(&self.image_available), None);
+
+        let image_index: u32;
+
+        match acquire_result {
+            Ok((acquired_index, suboptimal)) => {
+                image_index = acquired_index;
+
+                if suboptimal {
+                    todo!()
+                }
+            }
+            Err(result) => match result {
+                vk::Result::ERROR_OUT_OF_DATE_KHR => todo!(),
+                vk::Result::ERROR_SURFACE_LOST_KHR => todo!(),
+                vk::Result::ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT => todo!(),
+                _ => panic!("acquire_result = {:?}", result),
+            },
+        }
+
+        self.cmd_buf.reset();
+
+        self.record_commands(context, image_index);
+
+        let graphics_queue = context
+            .device
+            .get_first_queue(vk::QueueFlags::GRAPHICS)
+            .unwrap();
+
+        let present_queue = context.device.get_first_present_queue().unwrap();
+
+        graphics_queue.submit(
+            &[(
+                &self.image_available,
+                vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+            )],
+            &[&self.cmd_buf],
+            Some(&[&self.render_finished]),
+            Some(&self.in_flight),
+        );
+
+        let present_result =
+            present_queue.submit_present(&[&self.render_finished], &context.swapchain, image_index);
+
+        match present_result {
+            Ok(suboptimal) => {
+                if suboptimal {
+                    todo!()
+                }
+            }
+            Err(result) => match result {
+                vk::Result::ERROR_SURFACE_LOST_KHR => todo!(),
+                vk::Result::ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT => todo!(),
+                _ => panic!("present_result = {:?}", result),
+            },
+        }
+    }
+
+    pub fn record_commands(&self, context: &RenderContext, image_index: u32) {
+        self.cmd_buf.begin(vk::CommandBufferUsageFlags::empty());
+
+        let extent = context.swapchain.extent();
+        let framebuffer = context.framebuffers[image_index as usize].clone();
+
+        self.cmd_buf.begin_render_pass(
+            &context.render_pass,
+            &framebuffer,
+            vk::Rect2D {
+                offset: vk::Offset2D { x: 0, y: 0 },
+                extent: *extent,
+            },
+            Some(&[vk::ClearValue {
+                color: vk::ClearColorValue {
+                    float32: [0.0, 0.0, 0.0, 1.0],
+                },
+            }]),
+            vk::SubpassContents::INLINE,
+        );
+
+        self.cmd_buf.bind_pipeline(&context.graphics_pipeline);
+
+        self.cmd_buf.set_viewport(
+            0,
+            &[vk::Viewport {
+                x: 0.0,
+                y: 0.0,
+                width: extent.width as f32,
+                height: extent.height as f32,
+                min_depth: 0.0,
+                max_depth: 1.0,
+            }],
+        );
+
+        self.cmd_buf.set_scissor(
+            0,
+            &[vk::Rect2D {
+                offset: vk::Offset2D { x: 0, y: 0 },
+                extent: *extent,
+            }],
+        );
+
+        self.cmd_buf.draw(3, 1, 0, 0);
+        self.cmd_buf.end_render_pass();
+        self.cmd_buf.end();
+    }
+}
